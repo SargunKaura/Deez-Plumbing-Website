098e781fbf5c978696fdb06473fc00375b218e8e
Saif Ali <safesaif21@gmail.com>
2024-05-21 18:56:40 -0700

    Worked on html elements of Home Page and Our Mission Page along with creating a header for each page

--- deez-website/src/components/BookAppointment.js
+++ deez-website/src/components/BookAppointment.js
@@ -1,9 +1,28 @@
 import './BookAppointment.css';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 function BookAppointment() {
   return (
     <div className="App">
-      <h1>Book Appointment Page</h1>
+
+      <header>
+        <Link to="/">
+          <button>Home Page</button>
+        </Link>
+        <Link to="/">
+          <button>Contact Us</button>
+        </Link>
+        <Link to="/OurMission">
+          <button>Our Mission</button>
+        </Link>
+        <Link to="/BookAppointment">
+          <button>Book Appointment</button>
+        </Link>
+      </header>
+
+      <h1>Book an Appointment</h1>
+
+      <p>Fill out the form below to book an appointment with Deez Plumbing LTD. (might be an embeded google form) </p>
     </div>
   );
 }
