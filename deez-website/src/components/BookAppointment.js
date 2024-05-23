e19e4faa2752d2875c8d5860686fb96aba0a6589
Saif Ali <safesaif21@gmail.com>
2024-05-22 19:07:46 -0700

    added form functionality for BookAppointments Page

--- deez-website/src/components/BookAppointment.js
+++ deez-website/src/components/BookAppointment.js
@@ -3,7 +3,9 @@ import './BookAppointment.css';
 function BookAppointment() {
   return (
     <div className="App">
-      <h1>Book Appointment Page</h1>
+      <h1>Book an Appointment</h1>
+
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSccRrGin1zW39DCzn-aZ239D8fbHqTuR69ZClj5x7ZiRhqkGA/viewform?embedded=true" width="640" height="2000" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
   );
 }
