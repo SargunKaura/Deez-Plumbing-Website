0f5b8292169f00d0e5f17c91b28ae086cecc4322
Saif Ali <safesaif21@gmail.com>
2024-05-30 21:51:37 -0700

    Added button animations and cleaned up code structure on home page and a little on app.css

--- deez-website/src/components/Home.js
+++ deez-website/src/components/Home.js
@@ -1,171 +1,236 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../App.css'; // Adjust the path to where App.css is located
+import '../App.css';
 
 function Home() {
-  const scrollToContact = () => {
-    const contactSection = document.getElementById('contact-section');
-    if (contactSection) {
-      contactSection.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
+    const scrollToContact = () => {// function for making the contact us button scroll to the contact section
 
-  const images = [
-    '/images/default-image.jpg', // Use the path relative to the public directory
-    '/images/default-image.jpg',
-    '/images/default-image.jpg',
-    '/images/default-image.jpg', // Add more images as needed
-  ];
+        const contactSection = document.getElementById('contact-section');
 
-  const Carousel = ({ images }) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [transition, setTransition] = useState(true);
-    const totalSlides = Math.ceil(images.length / 2);
+        if (contactSection) {
 
-    const handleTransition = (newIndex) => {
-      setTransition(true);
-      setTimeout(() => {
-        setCurrentIndex(newIndex);
-      }, 500); // Match the duration with the CSS transition duration
+            contactSection.scrollIntoView({ behavior: 'smooth' });
+        }
     };
 
-    const nextSlide = () => {
-      handleTransition((currentIndex + 1) % totalSlides);
-    };
+    const images = [
 
-    const prevSlide = () => {
-      handleTransition(currentIndex === 0 ? totalSlides - 1 : currentIndex - 1);
+        '/images/default-image.jpg', //test images for now
+        '/images/default-image.jpg',
+        '/images/default-image.jpg',
+        '/images/default-image.jpg',
+    ];
+
+    const Carousel = ({ images }) => {
+
+        const [currentIndex, setCurrentIndex] = useState(0);
+        const [transition, setTransition] = useState(true);
+        const totalSlides = Math.ceil(images.length / 2);
+
+        const handleTransition = (newIndex) => {
+
+            setTransition(true);
+
+            setTimeout(() => {
+                setCurrentIndex(newIndex);
+            }, 500);
+        };
+
+        const nextSlide = () => {
+
+            handleTransition((currentIndex + 1) % totalSlides);
+        };
+
+        const prevSlide = () => {
+
+            handleTransition(currentIndex === 0 ? totalSlides - 1 : currentIndex - 1);
+        };
+
+        return (
+
+            <div className="carousel-div">
+
+                <button className="carousel-button" onClick={prevSlide}>
+                &#x2190; {/* Left arrow character code */}
+                </button>
+
+                <div className="carousel">
+
+                    <div className="carousel-slide" style={{ transform: `translateX(-${currentIndex * 100}%)`, transition: transition ? 'transform 0.3s ease-in-out' : 'none'}}>
+
+                        {images.map((image, index) => (
+                            <img key={index} src={image} alt={`Slide ${index}`} />
+                        ))}
+
+                    </div>
+
+                </div>
+
+                <button className="carousel-button" onClick={nextSlide}>
+                &#x2192; {/* Right arrow character code */}
+                </button>
+
+            </div>
+        );
     };
 
     return (
-      <div className="carousel-div">
-        <button className="carousel-button" onClick={prevSlide}>
-          &#x2190;  {/* Left arrow */}
-        </button>
-        <div className="carousel">
-        
-          <div className="carousel-slide"
-            style={{
-              transform: `translateX(-${currentIndex * 100}%)`,
-              transition: transition ? 'transform 0.3s ease-in-out' : 'none'
-            }}
-          >
-            {images.map((image, index) => (
-              <img key={index} src={image} alt={`Slide ${index}`} />
-            ))}
-          </div>
+
+        <div className="App">
+            <header className="header">
+
+                <div className="DPL-header">
+
+                    <Link to="/">
+                        <button className="DPL-header-button">DPL</button>
+                    </Link>
+
+                </div>
+
+                <div className="buttons-header-DIV">
+
+                    <Link to="/BookAppointment">
+                        <button className="header-button">Book an Appointment</button>
+                    </Link>
+
+                    <button className="header-button" onClick={scrollToContact}> Contact Us</button>
+
+                    <Link to="/OurMission">
+                        <button className="header-button">Our Mission</button>
+                    </Link>
+
+                </div>
+
+            </header>
+
+            <main>
+
+                <section className="title">
+
+                    <h2>Deez Plumbing LTD</h2>
+                    <p>Local Plumbers for Local People.</p>
+
+                    <Link to="/BookAppointment">
+                        <button className = "main-button" id = "appointment-button">Book an Appointment</button>
+                    </Link>
+
+                </section>
+
+                <section className="hours-operation">
+
+                    <div className="hours-content">
+
+                        <h3>Hours of Operation</h3>
+                        <p>Weekdays: 4:00 PM - 11:00 PM</p>
+                        <p>Weekends: 5:30 AM - 11:00 PM</p>
+                        <p>Holidays: CLOSED</p>
+
+                    </div>
+                    <div className="hours-image">
+
+                        <img src="plumber.jpg" alt="Plumber" />
+
+                    </div>
+
+                </section>
+
+                <section className="mission">
+
+                    <div className="mission-text">
+
+                        <h3>Our Mission</h3>
+                        <p>Two apprentice plumbers pursuing their dreams, founding their own
+                        company with a vision for excellence in plumbing services and
+                        customer satisfaction.</p>
+
+                    </div>
+
+                    <div className="mission-elements">
+
+                        <Link to="/OurMission">
+
+                            <button className = "main-button" id ="mission-button">Meet the Crew</button>
+                        </Link>
+
+                    </div>
+
+                </section>
+
+                <section className="reviews">
+
+                    <div className="reviews-text">
+
+                        <h3>Reviews and Testimonials</h3>
+                        <p>Our work and reviews.</p>
+
+                    </div>
+
+                    <Carousel images={images} />
+
+                    <div className="testimonials">
+
+                        <div className="testimonial">
+
+                            <p>"A heroic piece of praise!"</p>
+                            <p>- Name</p>
+
+                        </div>
+
+                        <div className="testimonial">
+
+                            <p>"A fantastic bit of feedback!"</p>
+                            <p>- Name</p>
+
+                        </div>
+
+                        <div className="testimonial">
+
+                            <p>"A genuinely glowing review!"</p>
+                            <p>- Name</p>
+
+                        </div>
+
+                    </div>
+
+                    <div className="review-button-Div">
+
+                        <button className = "main-button" id = "review-button">Read more Reviews</button>
+
+                    </div>
+
+                </section>
+
+                <section id="contact-section" className="contact">
+
+                    <h3>Contact Us</h3>
+                    <p>Email: Deezplumbers@gmail.com</p>
+                    <p>Business phone: TBD</p>
+
+                </section>
+
+            </main>
+
+            <footer className="footer">
+
+                <div className="DPL-footer">
+
+                  <p>Deez Plumbing LTD</p>
+
+                </div>
+
+                <nav>
+
+                    <div className="footer-links">
+                        <a href="#our-mission">Our Mission</a>
+                        <a href="#book-appointment">Book an Appointment</a>
+                    </div>
+
+                </nav>
+
+            </footer>
+
         </div>
-        <button className="carousel-button" onClick={nextSlide}>
-          &#x2192;  {/* Right arrow */}
-        </button>
-      </div>
     );
-  };
-
-  return (
-    <div className="App">
-      <header className="header">
-        <div className="DPL-header">
-          <Link to="/">
-            <button className="DPL-header-button">DPL</button>
-          </Link>
-        </div>
-
-        <div className="buttons-header-DIV">
-          <button className="header-button" onClick={scrollToContact}>
-            Contact Us
-          </button>
-          <Link to="/OurMission">
-            <button className="header-button">Our Mission</button>
-          </Link>
-          <Link to="/BookAppointment">
-            <button className="header-button">Book an Appointment</button>
-          </Link>
-        </div>
-      </header>
-
-      <main>
-        <section className="title">
-          <h2>Deez Plumbing LTD</h2>
-          <p>Local Plumbers for Local People.</p>
-          <Link to="/BookAppointment">
-            <button className="appointment-button">Book an Appointment</button>
-          </Link>
-        </section>
-
-        <section className="hours-operation">
-          <div className="hours-content">
-            <h3>Hours of Operation</h3>
-            <p>Weekdays: 4:00 PM - 11:00 PM</p>
-            <p>Weekends: 5:30 AM - 11:00 PM</p>
-            <p>Holidays: CLOSED</p>
-          </div>
-          <div className="hours-image">
-            <img src="plumber.jpg" alt="Plumber" />
-          </div>
-        </section>
-
-        <section className="mission">
-          <div className="mission-text">
-            <h3>Our Mission</h3>
-            <p>
-              Two apprentice plumbers pursuing their dreams, founding their own
-              company with a vision for excellence in plumbing services and
-              customer satisfaction.
-            </p>
-          </div>
-          <div className="mission-elements">
-            <Link to="/OurMission">
-              <button className="mission-button">Meet the Crew</button>
-            </Link>
-          </div>
-        </section>
-
-        <section className="reviews">
-          <div className="reviews-text">
-            <h3>Reviews and Testimonials</h3>
-            <p>Our work and reviews.</p>
-          </div>
-          <Carousel images={images} />
-          <div className="testimonials">
-            <div className="testimonial">
-              <p>"A heroic piece of praise!"</p>
-              <p>- Name</p>
-            </div>
-            <div className="testimonial">
-              <p>"A fantastic bit of feedback!"</p>
-              <p>- Name</p>
-            </div>
-            <div className="testimonial">
-              <p>"A genuinely glowing review!"</p>
-              <p>- Name</p>
-            </div>
-          </div>
-          <div className="review-button-Div">
-            <button className="review-button">Read more Reviews</button>
-          </div>
-        </section>
-
-        <section id="contact-section" className="contact">
-          <h3>Contact Us</h3>
-          <p>Email: Deezplumbers@gmail.com</p>
-          <p>Business phone: TBD</p>
-        </section>
-      </main>
-
-      <footer className="footer">
-        <div className="DPL-footer">
-          <p>Deez Plumbing LTD</p>
-        </div>
-        <nav>
-          <div className="footer-links">
-            <a href="#our-mission">Our Mission</a>
-            <a href="#book-appointment">Book an Appointment</a>
-          </div>
-        </nav>
-      </footer>
-    </div>
-  );
 }
 
 export default Home;
