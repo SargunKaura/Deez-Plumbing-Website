ed140ce244ba47178ee1097be1e7d95688f82af8
Saif Ali <safesaif21@gmail.com>
2024-06-04 18:56:57 -0700

    Improved/cleaned BookAppointments.js code structure

--- deez-website/src/components/BookAppointment.js
+++ deez-website/src/components/BookAppointment.js
@@ -2,52 +2,58 @@ import '../App.css';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 function BookAppointment() {
-  return (
-    <div className="App">
 
-        <header className="header">
+    return (
+        <div className="App">
 
-        <div className="DPL-header">
+            <header className="header">
+                <div className="DPL-header">
 
-            <Link to="/">
-                <button className="DPL-header-button">DPL</button>
-            </Link>
+                    <Link to="/">
+                    <button className="DPL-header-button">DPL</button>
+                    </Link>
 
+                </div>
+
+                <div className="buttons-header-DIV">
+
+                    <Link to="/BookAppointment">
+                    <button className="header-button">Book an Appointment</button>
+                    </Link>
+
+                    <Link to="/">
+                    <button className="header-button"> Contact Us</button>
+                    </Link>
+
+                    <Link to="/OurMission">
+                    <button className="header-button">Our Mission</button>
+                    </Link>
+
+                </div>
+            </header>
+
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSccRrGin1zW39DCzn-aZ239D8fbHqTuR69ZClj5x7ZiRhqkGA/viewform?embedded=true" width="640" height="1800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            
+            <footer className="footer">
+
+                <div className="DPL-footer">
+
+                    <p>Deez Plumbing LTD</p>
+
+                </div>
+
+                <nav>
+
+                    <div className="footer-links">
+
+                        <a href="#our-mission">Our Mission</a>
+                        <a href="#book-appointment">Book an Appointment</a>
+                        
+                    </div>
+                </nav>
+            </footer>
         </div>
-
-        <div className="buttons-header-DIV">
-
-            <Link to="/BookAppointment">
-                <button className="header-button">Book an Appointment</button>
-            </Link>
-
-            <Link to="/">
-            <button className="header-button"> Contact Us</button>
-            </Link>
-
-
-            <Link to="/OurMission">
-                <button className="header-button">Our Mission</button>
-            </Link>
-
-        </div>
-
-        </header>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSccRrGin1zW39DCzn-aZ239D8fbHqTuR69ZClj5x7ZiRhqkGA/viewform?embedded=true" width="640" height="1800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-    <footer className="footer">
-      <div className="DPL-footer">
-        <p>Deez Plumbing LTD</p>
-      </div>
-      <nav>
-        <div className="footer-links">
-          <a href="#our-mission">Our Mission</a>
-          <a href="#book-appointment">Book an Appointment</a>
-        </div>
-      </nav>
-    </footer>
-    
-    </div>
-  );
+    );
 }
 
 export default BookAppointment;
